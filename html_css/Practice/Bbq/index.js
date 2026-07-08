5c40c3ad15ca7699f3a6a4a44e86e1d86e445ba8
Santosh Sunuwar Mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-07-08 12:30:02 +0545

    added logic to the form

--- html_css/Practice/Bbq/index.js
+++ html_css/Practice/Bbq/index.js
@@ -1,25 +1,76 @@
-// let mark = 80;
-
-// if (mark < 40) {
-//     console.log("Fail");
-// } else if (mark >= 40 && mark < 60) {
-//     console.log("C");
-// } else if (mark >= 60 && mark < 80) {
-//     console.log("B");
-// } else {
-//     console.log("A");
-// }
+// 1. SELECT ELEMENTS OUTSIDE (Happens once when page loads)
+const form = document.getElementById('registerForm');
+let usernameInput = document.getElementById('username');
+let emailInput = document.getElementById('email');
+let passwordInput = document.getElementById('password');
+let confirmPasswordInput = document.getElementById('confirm-password');
 
 
-for(let num = 0; num < 21; num ++){
-    if (num % 3 === 0 && num % 5 === 0) {
-        console.log(num, "FizzBuzz");
-    } else if (num % 3 === 0) {
-        console.log(num, "Fizz");
-    } else if (num % 5 === 0) {
-        console.log(num, "Buzz");
-    } else {
-        console.log(num, "Nothing.");
+
+let usernameError = document.getElementById('username-error');
+let emailError = document.getElementById('email-error');
+let passwordError = document.getElementById('password-error');
+let confirmPasswordError = document.getElementById('confirm-password-error');
+
+
+form.addEventListener('submit', function (e) {
+    // Stop the page from reloading automatically
+    e.preventDefault();
+
+    let isFormValid = true;
+
+    usernameError.innerText = "";
+    emailError.innerText = "";
+    passwordError.innerText = "";
+    confirmPasswordError.innerText = "";
+
+    let usernameValue = usernameInput.value;
+    let emailValue = emailInput.value;
+    let passwordValue = passwordInput.value;
+    let confirmPasswordValue = confirmPasswordInput.value;
+
+    // User Validation
+    if (usernameValue === "") {
+        usernameError.innerText = "Username is required.";
+        isFormValid = false;
+    } else if (usernameValue.length <= 3 || usernameValue.length >= 15) {
+        usernameError.innerText = "Username must be more than 3 and less than 15 characters.";
+        isFormValid = false;
     }
 
-}
+    // Email Validation
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailValue === "") {
+        emailError.innerText = "Email is required.";
+        isFormValid = false;
+    } else if (emailPattern.test(emailValue) === false) {
+        emailError.innerText = "Please enter a valid email address.";
+        isFormValid = false;
+    }
+
+    // Password Validation
+    if (passwordValue === "") {
+        passwordError.innerText = "Password is required.";
+        isFormValid = false;
+    } else if (passwordValue.length < 4 || passwordValue.length > 8) {
+        passwordError.innerText = 'Password must be more than 3 and less than 8 characters.'
+        isFormValid = false;
+    }
+
+    // Password Confirmation 
+    if (confirmPasswordValue === "") {
+        confirmPasswordError.innerText = "Confirm password is required.";
+        isFormValid = false;
+    } else if (passwordValue !== confirmPasswordValue) {
+        confirmPasswordError.innerText = "Passwords do not match.";
+        isFormValid = false;
+    }
+
+    if (isFormValid === true) {
+        usernameInput.value = ''
+        emailInput.value = ''
+        passwordInput.value = ''
+        confirmPasswordInput.value = ''
+        alert('Registration Successful!')
+    }
+});
